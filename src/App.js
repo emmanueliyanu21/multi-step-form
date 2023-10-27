3ef1494bdeadc582f6b3e884fa5367d374202b95
Emmanueliyanu21 <emmanuel.iyanu@kaiglo.com>
2023-10-27 23:57:49 +0100

    refactoring the interface and functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,117 @@
 import './App.css';
 import React, { useState } from 'react';
 
+import sideImage from './assets/images/bg-sidebar-desktop.svg'
+import Arcade from './assets/images/icon-arcade.svg'
+import Advanced from './assets/images/icon-advanced.svg'
+import Pro from './assets/images/icon-pro.svg'
+
 function App() {
-  const [step, setStep] = useState(0); // Initialize with the current step
+  const [step, setStep] = useState(0);
+  const [isChecked, setIsCheceked] = useState(false);
 
-  const nextStep = () => {
-    if (step >= steps.length) {
-      return;
-    }
-    setStep(step + 1);
+  const changeMenu = (tabIndex) => {
+    setStep(tabIndex);
   };
 
-  const prevStep = () => {
-    if (step > 0) {
-      setStep(step - 1);
-    }
-  };
+  const changeCheckBox = (e) => {
+    setIsCheceked(e.target.checked)
+  }
 
-  const changeMenu = (menuStep) => {
-    setStep(menuStep);
-  };
-
-  const steps = [
+  const menuItems = [
+    {
+      number: 1,
+      step: 'YOUR INFO',
+      title: 'STEP 1',
+    },
+    {
+      number: 2,
+      step: 'SELECT PLAN',
+      title: 'STEP 2',
+    },
+    {
+      number: 3,
+      step: 'ADD-ONS',
+      title: 'STEP 3',
+    },
+    {
+      number: 4,
+      step: 'SUMMARY',
+      title: 'STEP 4',
+    },
   ];
+
+  const formFields = [
+    {
+      label: 'Name',
+      type: 'text',
+      placeholder: 'e.g. Stephen King',
+    },
+    {
+      label: 'Email Address',
+      type: 'text',
+      placeholder: 'e.g. stephenking@lorem.com',
+    },
+    {
+      label: 'Phone Number',
+      type: 'text',
+      placeholder: 'e.g. +1 234 567 890',
+    },
+  ];
+
+  const cardData = [
+    {
+      title: 'Arcade',
+      imageSrc: Arcade,
+      price: '$9/month',
+    },
+    {
+      title: 'Advanced',
+      imageSrc: Advanced,
+      price: '$12/month',
+    },
+    {
+      title: 'Pro',
+      imageSrc: Pro,
+      price: '$15/month',
+    },
+  ];
+
+  const addOnsData = [
+    {
+      title: 'Online service',
+      description: 'Access to multiplayer games',
+      price: '+$1/mo',
+    },
+    {
+      title: 'Larger storage',
+      description: 'Extra 1TB of cloud save',
+      price: '+$2/mo',
+    },
+    {
+      title: 'Customizable profile',
+      description: 'Custom theme on your profile',
+      price: '+$5/mo',
+    },
+  ];
+
+  const cardDatas = [
+    {
+      title: 'Arcade (Monthly)',
+      change: 'Change',
+      amount: '+$9/mo',
+    },
+    {
+      title: 'Online service',
+      amount: '+$1/mo',
+    },
+    {
+      title: 'Larger storage',
+      amount: '+$1/mo',
+    },
+  ];
+
+
 
   return (
     <>
@@ -30,82 +119,53 @@ function App() {
         <div className="body-content grid-container">
           <div className="content-wrapper">
             <div className="menu-wrapper">
-              <div className="nav" onClick={changeMenu}>
-                <div className="number"><span className="active">1</span></div>
-                <div className="info">
-                  <h3>STEP 1</h3>
-                  <h2>YOUR INFO</h2>
-                </div>
-              </div>
-              <div className="nav" onClick={changeMenu}>
-                <div className="number"><span>2</span></div>
-                <div className="info">
-                  <h3>STEP 2</h3>
-                  <h2>SELECT PLAN</h2>
-                </div>
-              </div>
-              <div className="nav" onClick={changeMenu}>
-                <div className="number"><span>3</span></div>
-                <div className="info">
-                  <h3>STEP 3</h3>
-                  <h2>ADD-ONS</h2>
-                </div>
-              </div>
-              <div className="nav" onClick={changeMenu}>
-                <div className="number"><span>4</span></div>
-                <div className="info">
-                  <h3>STEP 4</h3>
-                  <h2>SUMMARY</h2>
-                </div>
-              </div>
+              {
+                menuItems.map((item, index) => (
+                  <div className="nav" key={index} onClick={() => changeMenu(index)}>
+                    <div className="number"><span className={index === step ? 'active' : ''}>{item?.number}</span></div>
+                    <div className="info">
+                      <h3>{item?.step}</h3>
+                      <h2>{item?.title}</h2>
+                    </div>
+                  </div>
+                ))
+              }
               <div className="side-image">
-                <img src="./assets/images/bg-sidebar-desktop.svg" alt="" />
+                <img src={sideImage} alt="" />
               </div>
             </div>
             <div className="form-wrapper">
-              <div id="step1" className="step active">
+              {step === 0 && <div id="step1" className="step active">
                 <div className="header">
                   <h2>Personal Info</h2>
                   <p>Please provide your name, email address, and phone number.</p>
                 </div>
-                <div className="content">
-                  <label htmlFor="">Name</label><br />
-                  <input type="text" placeholder="e.g. Stephen King" />
-                </div>
-                <div className="content">
-                  <label htmlFor="">Email Address</label> <br />
-                  <input type="text" placeholder="e.g. stephenking@lorem.com" />
-                </div>
-                <div className="content">
-                  <label htmlFor="">Phone Number</label> <br />
-                  <input type="text" placeholder="e.g. +1 234 567 890" />
-                </div>
+                {
+                  formFields.map((field, index) => (
+                    <div key={index} className="content">
+                      <label htmlFor={field.label}>{field.label}</label><br />
+                      <input type={field.type} placeholder={field.placeholder} />
+                    </div>
+                  ))
+                }
                 <div className="next-step first">
-                  <button className="btn" type="button" onClick={nextStep}>Next Step</button>
+                  <button className="btn" type="button" onClick={() => changeMenu(1)}>Next Step</button>
                 </div>
-              </div>
-              <div className="step">
+              </div>}
+              {step === 1 && <div className="step active">
                 <div className="header">
                   <h2>Select your plan</h2>
                   <p>You have the option of monthly or yearly billing.</p>
                 </div>
                 <div className="content">
                   <div className="card-wrapper">
-                    <div className="content-title active">
-                      <img src="./assets/images/icon-arcade.svg" alt="" />
-                      <h2>Arcade</h2>
-                      <p>$9/month</p>
-                    </div>
-                    <div className="content-title">
-                      <img src="./assets/images/icon-advanced.svg" alt="" />
-                      <h2>Advanced</h2>
-                      <p>$12/month</p>
-                    </div>
-                    <div className="content-title">
-                      <img src="./assets/images/icon-pro.svg" alt="" />
-                      <h2>Pro</h2>
-                      <p>$15/month</p>
-                    </div>
+                    {cardData.map((card, index) => (
+                      <div key={index} className={`content-title ${index === 0 ? 'active' : ''}`}>
+                        <img src={card.imageSrc} alt="" />
+                        <h2>{card.title}</h2>
+                        <p>{card.price}</p>
+                      </div>
+                    ))}
                   </div>
                   <div className="monthly-yearly">
                     <div>
@@ -117,101 +177,66 @@ function App() {
                       <span className="monthly-yearly-text">Yearly</span>
                     </div>
                   </div>
-                  <div className="next-step">
-                    <button onClick={prevStep} className="btn back">Go Back</button>
-                    <button className="btn" type="button" onClick={nextStep}>Next Step</button>
-                  </div>
                 </div>
-              </div>
-              <div className="step">
+                <div className="next-step">
+                    <button onClick={() => changeMenu(0)} className="btn back">Go Back</button>
+                    <button className="btn" type="button" onClick={() => changeMenu(2)}>Next Step</button>
+                  </div>
+              </div>}
+              {step === 2 && <div className="step active">
                 <div className="header">
                   <h2>Pick add-ons</h2>
                   <p>Add-ons help enhance your gaming experience.</p>
                 </div>
                 <div className="content">
                   <div className="card-wrapper add-ons">
-                    <div className="content-title active">
-                      <div className="content-title-details">
-                        <div className="checkbox">
-                          <input type="checkbox" id="scales" name="scales" checked />
+                    {addOnsData.map((addOn, index) => (
+                      <div key={index} className={`content-title ${index === 0 ? 'active' : ''}`}>
+                        <div className="content-title-details">
+                          <div className="checkbox">
+                            <input
+                              type="checkbox"
+                              id={`scales${index}`}
+                              name={`scales${index}`}
+                              checked={isChecked}
+                              onChange={changeCheckBox}
+                            />
+                          </div>
+                          <div className="inner-text">
+                            <h2>{addOn.title}</h2>
+                            <p>{addOn.description}</p>
+                          </div>
                         </div>
-                        <div className="inner-text">
-                          <h2>Online service</h2>
-                          <p>Access to multiplayer games</p>
-                        </div>
-                      </div>
-                      <div className="amount">
-                        <p>+$1/mo</p>
-                      </div>
-                    </div>
-                    <div className="content-title active">
-                      <div className="content-title-details">
-                        <div className="checkbox">
-                          <input type="checkbox" id="scales" name="scales" checked />
-                        </div>
-                        <div className="inner-text">
-                          <h2>Larger storage</h2>
-                          <p>Extra 1TB of cloud save</p>
+                        <div className="amount">
+                          <p>{addOn.price}</p>
                         </div>
                       </div>
-                      <div className="amount">
-                        <p>+$2/mo</p>
-                      </div>
-                    </div>
-                    <div className="content-title ">
-                      <div className="content-title-details">
-                        <div className="checkbox">
-                          <input type="checkbox" id="scales" name="scales" checked />
-                        </div>
-                        <div className="inner-text">
-                          <h2>Customizable profile</h2>
-                          <p>Custom theme on your profile</p>
-                        </div>
-                      </div>
-                      <div className="amount">
-                        <p>+$2/mo</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="next-step">
-                    <button onClick={prevStep} className="btn back">Go Back</button>
-                    <button className="btn" type="button" onClick={nextStep}>Next Step</button>
+                    ))}
                   </div>
                 </div>
-              </div>
-              <div className="step">
+                <div className="next-step">
+                    <button onClick={() => changeMenu(1)} className="btn back">Go Back</button>
+                    <button className="btn" type="button" onClick={() => changeMenu(3)}>Next Step</button>
+                  </div>
+              </div>}
+              {step === 3 && <div className="step active">
                 <div className="header">
                   <h2>Finishing up</h2>
                   <p>Double-check everything looks OK before confirming.</p>
                 </div>
                 <div className="content">
                   <div className="card-wrapper add-ons finishing-up">
-                    <div className="content-title">
-                      <div className="inner-text">
-                        <h2>Arcade (Monthly)</h2>
-                        <p>Change</p>
+                    {cardDatas.map((card, index) => (
+                      <div key={index} className="content-title">
+                        <div className="inner-text">
+                          {card.change ? <h2>{card.title}</h2> : <p>{card.title}</p>}
+                          {card.change && <p>{card.change}</p>}
+                        </div>
+                        <div className="amount">
+                          <p>{card.amount}</p>
+                        </div>
                       </div>
-                      <div className="amount">
-                        <h2>+$9/mo</h2>
-                      </div>
-                    </div>
-                    <hr />
-                    <div className="content-title">
-                      <div className="inner-text">
-                        <p>Online service</p>
-                      </div>
-                      <div className="amount">
-                        <p>+$1/mo</p>
-                      </div>
-                    </div>
-                    <div className="content-title">
-                      <div className="inner-text">
-                        <p>Larger storage</p>
-                      </div>
-                      <div className="amount">
-                        <p>+$1/mo</p>
-                      </div>
-                    </div>
+                    ))}
                   </div>
                   <div className="total add-ons">
                     <div className="content-title">
@@ -223,12 +248,12 @@ function App() {
                       </div>
                     </div>
                   </div>
-                  <div className="next-step total">
-                    <button onClick={prevStep} className="btn back">Go Back</button>
+                </div>
+                <div className="next-step total">
+                    <button onClick={() => changeMenu(2)} className="btn back">Go Back</button>
                     <button className="btn" type="button">Confirm</button>
                   </div>
-                </div>
-              </div>
+              </div>}
             </div>
           </div>
         </div>
